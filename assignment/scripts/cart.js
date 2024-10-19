a9fa8d19bdcf6437478bdf205153cf9c7a93c766
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-10-19 11:51:02 -0500

    created basket global variable

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,11 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+console.log(basket);
 
-
-
-
-
+function addItem(item){
+if (basket.length < 3) {basket.push(item);
+return true + item;
+}
+}
+console.log(`Adding chips and queso`, additem(`chips and queso`));
 
 
 
